1426f05e8cc5a7526c38a26f5dc5deea23e24ebc
MiguelFrBarreto <miguelfrbarreto@gmail.com>
2025-12-18 10:17:58 -0300

    Add User, Expense and Category repositories

---
+++ Expense-Tracker/src/main/java/com/example/ExpenseTracker/expense/ExpenseRepository.java
@@ -0,0 +1,7 @@
+package com.example.ExpenseTracker.expense;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    
+}
